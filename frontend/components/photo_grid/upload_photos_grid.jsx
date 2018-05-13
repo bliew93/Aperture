9bfd774602a75707f54a1b48479635035d04c10e
Brian Liew <brianliew93@gmail.com>
2018-05-13 15:49:12 -0400

    Add grid styling to photo grid component

--- frontend/components/photo_grid/upload_photos_grid.jsx
+++ frontend/components/photo_grid/upload_photos_grid.jsx
@@ -6,7 +6,7 @@ const UploadPhotoGrid = ({ photos, updateSelectedPhoto, selectedPhoto }) => {
     let selected;
     selectedPhoto === photo.id ? selected = '-selected' : selected = ''
     return (
-      <div className={`photo-item${selected}`} key={idx}>
+      <div className={`photo-grid-item${selected}`} key={idx}>
         <a className="photo-link" href='#' onClick={() => updateSelectedPhoto(photo.id)}>
           <img src={photo.imageUrl}></img>
         </a>
